d3f6c80c5f51d6b0cf82ffe79ecc5a2dd90a8791
Avdi Grimm <avdi@avdi.org>
2013-05-09 16:44:21 +0200

    Basic randomness test

--- spec/lottery_spec.rb
+++ spec/lottery_spec.rb
@@ -7,9 +7,25 @@ describe 'lottery app' do
   end
 
   specify 'choosing a single winner' do
-    run "raffle spec/5names pick 'POODR'"
+    run "raffle spec/5names 'POODR'"
     expect(output_lines).to have(1).line
     expect(names_from('spec/5names')).to include(output_lines[0])
+  end
+
+  specify 'randomness' do
+    winners = []
+    100.times do
+      run "raffle spec/5names 'POODR'"
+      winners << output_lines.first
+      FileUtils.rm_f Dir.glob('spec/*.raffle')
+    end
+
+    edwina_count = winners.count('Edwina Gusikowski')
+    expect(edwina_count / 100.0).to be_within(0.1).of(0.2)
+  end
+
+  specify 'remembering winners' do
+    run "raffle spec/5names 'POODR' --force '0,1,2,3,4'"
   end
 
   def run(command)
